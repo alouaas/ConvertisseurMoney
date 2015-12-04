d0e7d056ee6364b7d14f876ead7e540972ee1e6c
alouaas <aurelien.louaas@gmail.com>
2015-12-04 21:53:19 +0100

    projet final

--- src/main/java/Convertisseur.java
+++ src/main/java/Convertisseur.java
@@ -12,9 +12,9 @@ public class Convertisseur extends JFrame implements ActionListener {
         new Convertisseur().setVisible(true);
     }
 
-    String []dv={"Devise"};
-    String []unt={"Euro (EUR)","Dollar US (USD)","Livre Sterling (GBP)"};
-    JFrame f=new JFrame("Convertisseur");
+
+    String []unt={"Euro (EUR)","Dollar US (USD)","Livre Sterling (GBP)","Yen Japonais (JPY)"};
+    JFrame f=new JFrame("ConvertisseurMoney");
     JButton bcal=new JButton("Calculer");
     JButton bexit=new JButton("Quitter");
 
@@ -23,42 +23,44 @@ public class Convertisseur extends JFrame implements ActionListener {
     JPanel p2=new JPanel();
     JPanel p3=new JPanel();
 
-    JLabel lblmesu=new JLabel("Mesure:");
+    JLabel lblmesu=new JLabel("Bienvenue dans mon Convertisseur de Devises");
+
+    JLabel lblunite=new JLabel("Veuillez selectionner vos devises");
     JLabel lblunite1=new JLabel("Unite:");
     JLabel lblunite2=new JLabel("Unite:");
 
-    JComboBox mesu1=new JComboBox(dv);
+
     JComboBox unite1=new JComboBox();
     JComboBox unite2=new JComboBox();
 
-    JTextField textun1=new JTextField();
-    JTextField textun2=new JTextField();
-    JMenuBar jmenubar1=new JMenuBar();
-    JMenu jmenu1=new JMenu("?");
-    JMenuItem jmenuitem1=new JMenuItem("Apropos");
+    JTextField textuni1=new JTextField();
+    JTextField textuni2=new JTextField();
+    JMenuBar jmenubar=new JMenuBar();
+    JMenu jmenu=new JMenu("?");
+    JMenuItem jmenuitem=new JMenuItem("Apropos");
 
 
     public Convertisseur(){
         this.setTitle("Convertisseur");
-        this.setSize(new Dimension(550,350));
-        this.setJMenuBar(jmenubar1);
-        jmenubar1.add(jmenu1);
-        jmenu1.add(jmenuitem1);
+        this.setSize(new Dimension(600,400));
+        this.setJMenuBar(jmenubar);
+        jmenubar.add(jmenu);
+        jmenu.add(jmenuitem);
 
         this.add(p);
         p.setLayout(new GridLayout(7,1));
         p.add(lblmesu);
-        p.add(mesu1);
+        p.add(lblunite);
         p.add(p1);
         p1.setLayout(new GridLayout(1,2));
         p1.add(lblunite1);
         p1.add(unite1);
-        p.add(textun1);
+        p.add(textuni1);
         p.add(p2);
         p2.setLayout(new GridLayout(1,2));
         p2.add(lblunite2);
         p2.add(unite2);
-        p.add(textun2);
+        p.add(textuni2);
         p3.setLayout(new GridLayout(1,2));
         p3.add(bcal);
         p3.add(bexit);
@@ -66,10 +68,9 @@ public class Convertisseur extends JFrame implements ActionListener {
 
         bexit.addActionListener(this);
         bcal.addActionListener(this);
-        mesu1.addActionListener(this);
         unite1.addActionListener(this);
         unite2.addActionListener(this);
-        jmenuitem1.addActionListener(this);
+        jmenuitem.addActionListener(this);
     }
 
     WindowAdapter win=new WindowAdapter() {
@@ -79,46 +80,93 @@ public class Convertisseur extends JFrame implements ActionListener {
 
     WindowAdapter win1=new WindowAdapter() {
         public void windowOpened(WindowEvent e){
-            for(int i=0;i<3;i++)
+            for(int i=0;i<4;i++)
                 unite1.addItem(unt[i]);
-            for(int i=2;i>-1;i--)
+            for(int i=0;i<4;i++)
                 unite2.addItem(unt[i]);
         }
     };
     {this.addWindowListener(win1);}
 
     public void actionPerformed(ActionEvent e){
-        if(e.getSource()==jmenuitem1)
-            JOptionPane.showMessageDialog(this,"by LOUAAS Aurelien","Menus",JOptionPane.INFORMATION_MESSAGE);
+        if(e.getSource()==jmenuitem)
+            JOptionPane.showMessageDialog(this,"by LOUAASAurelien/MIF2/ESILV","Details",JOptionPane.INFORMATION_MESSAGE);
         if(e.getSource()==bexit)
             System.exit(0);
         if(e.getSource()==bcal)
         {
-            double s=Double.valueOf(textun1.getText()).doubleValue();
-            if(mesu1.getSelectedIndex()==0)
+            double s=Double.valueOf(textuni1.getText()).doubleValue();
+            if(unite1.getSelectedIndex()==0)
             {
-                if(unite1.getSelectedIndex()==0)
+                if(unite2.getSelectedIndex()==1)
                 {
                     s=s*1.0609;
-                    textun2.setText(""+s);
+                    textuni2.setText(""+s);
                 }
-                else if(unite1.getSelectedIndex()==1)
+                else if(unite2.getSelectedIndex()==2)
                 {
-                    s=s/1.0609;
-                    textun2.setText(""+s);
+                    s=s*0.7229;
+                    textuni2.setText(""+s);
+                }
+                else if(unite2.getSelectedIndex()==3)
+                {
+                    s=s*134.2042;
+                    textuni2.setText(""+s);
                 }
             }
-        }
-        if(e.getSource()==mesu1)
-        {
-            if(mesu1.getSelectedIndex()==0)
+            if(unite1.getSelectedIndex()==1)
             {
-                unite1.removeAllItems();
-                unite2.removeAllItems();
-                for(int i=0;i<3;i++)
-                    unite1.addItem(unt[i]);
-                for(int i=2;i>-1;i--)
-                    unite2.addItem(unt[i]);
+                if(unite2.getSelectedIndex()==0)
+                {
+                    s=s/1.0609;
+                    textuni2.setText(""+s);
+                }
+                else if(unite2.getSelectedIndex()==2)
+                {
+                    s=s*0.6627;
+                    textuni2.setText(""+s);
+                }
+                else if(unite2.getSelectedIndex()==3)
+                {
+                    s=s*123.0215;
+                    textuni2.setText(""+s);
+                }
+            }
+            if(unite1.getSelectedIndex()==2)
+            {
+                if(unite2.getSelectedIndex()==0)
+                {
+                    s=s/0.7229;
+                    textuni2.setText(""+s);
+                }
+                else if(unite2.getSelectedIndex()==1)
+                {
+                    s=s/0.6627;
+                    textuni2.setText(""+s);
+                }
+                else if(unite2.getSelectedIndex()==3)
+                {
+                    s=s*185.6371;
+                    textuni2.setText(""+s);
+                }
+            }
+            if(unite1.getSelectedIndex()==3)
+            {
+                if(unite2.getSelectedIndex()==0)
+                {
+                    s=s/134.2042;
+                    textuni2.setText(""+s);
+                }
+                else if(unite2.getSelectedIndex()==1)
+                {
+                    s=s/123.0215;
+                    textuni2.setText(""+s);
+                }
+                else if(unite2.getSelectedIndex()==2)
+                {
+                    s=s/185.6371;
+                    textuni2.setText(""+s);
+                }
             }
         }
     }
